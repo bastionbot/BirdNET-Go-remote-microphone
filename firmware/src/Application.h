035f1551f04c7b5e220dec196854328508b70cb2
bastionbot <droneship16@gmail.com>
2025-08-04 23:16:28 -0400

    Update Application.h

--- firmware/src/Application.h
+++ firmware/src/Application.h
@@ -1,16 +1,22 @@
 #pragma once
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 class Transport;
 class I2SSampler;
 
 class Application
 {
 private:
-  Transport *transport1 = NULL;
-  Transport *transport2 = NULL;
-  I2SSampler *input = NULL;
-
+    Transport *transport1 = NULL;
+    Transport *transport2 = NULL;
+    I2SSampler *input = NULL;
+    TaskHandle_t taskHandle = NULL;
+    volatile bool taskRunning = false;
+    
 public:
-  void begin();
-  static void streamer_task(void *param);
+    void begin();
+    ~Application();  // Destructor
+    static void streamer_task(void *param);
 };
