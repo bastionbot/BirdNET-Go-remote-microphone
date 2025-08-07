fafe0ce4ac1ae0e90b62aa8bca77abc43134ff77
bastionbot <droneship16@gmail.com>
2025-08-06 22:14:29 -0400

    Update main.cpp
    fixed wifi reconnection and application restart task

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,18 +2,22 @@
 #include <WiFi.h>
 #include "Application.h"
 #include "config.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 unsigned long lastReconnectAttempt = 0;
 const unsigned long reconnectInterval = 10000; // 10 seconds
 Application *application = nullptr;
 bool wasConnected = false;
 
+void wifiWatchdog( void *pvParameters );
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Starting up");
   delay(1000);
-  
+
   // WiFi.onEvent(WiFiEvent);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
@@ -21,7 +25,7 @@ void setup()
     delay(500);
     Serial.print(".");
   }
-  
+
   // disable WiFi sleep mode
   WiFi.setSleep(WIFI_PS_NONE);
   Serial.println("");
@@ -29,59 +33,67 @@ void setup()
   Serial.println("IP address: ");
   Serial.print(WiFi.localIP());
   Serial.println("");
-  
+
   Serial.println("Creating microphone");
   application = new Application();
   application->begin();
   wasConnected = true;
+  xTaskCreate(wifiWatchdog, "WiFi Watchdog", 4096, NULL, 1, NULL);
 }
+
 
 void loop()
 {
-  static wl_status_t lastStatus = WL_CONNECTED;
-  wl_status_t currentStatus = WiFi.status();
-  
-  if (currentStatus != lastStatus) {
-    Serial.print("[WiFi] Status changed: ");
-    Serial.println(currentStatus);
-    lastStatus = currentStatus;
-  }
-  
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    if (wasConnected) {
-      Serial.println("[WiFi] Connection lost - stopping application");
+}
+
+void wifiWatchdog( void *pvParameters )
+{
+  while(true) {
+    static wl_status_t lastStatus = WL_CONNECTED;
+    wl_status_t currentStatus = WiFi.status();
+
+    if (currentStatus != lastStatus) {
+      Serial.print("[WiFi] Status changed: ");
+      Serial.println(currentStatus);
+      Serial.flush();
+      lastStatus = currentStatus;
+    }
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      if (wasConnected) {
+        Serial.println("[WiFi] Connection lost - stopping application");
+        if (application) {
+          delete application;
+          application = nullptr;
+        }
+        wasConnected = false;
+      }
+
+      unsigned long now = millis();
+      if (now - lastReconnectAttempt > reconnectInterval)
+      {
+        Serial.println("[WiFi] Reconnecting...");
+        WiFi.disconnect();
+        WiFi.reconnect();
+        lastReconnectAttempt = now;
+      }
+      vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+    else if (!wasConnected) {
+      // WiFi just reconnected
+      Serial.println("[WiFi] Reconnected - restarting application");
+      Serial.println("IP address: ");
+      Serial.print(WiFi.localIP());
+      Serial.flush();
+
       if (application) {
         delete application;
-        application = nullptr;
       }
-      wasConnected = false;
+      application = new Application();
+      application->begin();
+      wasConnected = true;
     }
-    
-    unsigned long now = millis();
-    if (now - lastReconnectAttempt > reconnectInterval)
-    {
-      Serial.println("[WiFi] Reconnecting...");
-      WiFi.disconnect();
-      WiFi.reconnect();
-      lastReconnectAttempt = now;
-    }
-    vTaskDelay(pdMS_TO_TICKS(5000));
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
-  else if (!wasConnected) {
-    // WiFi just reconnected
-    Serial.println("[WiFi] Reconnected - restarting application");
-    Serial.println("IP address: ");
-    Serial.print(WiFi.localIP());
-    Serial.println("");
-    
-    if (application) {
-      delete application;
-    }
-    application = new Application();
-    application->begin();
-    wasConnected = true;
-  }
-  
-  vTaskDelay(pdMS_TO_TICKS(1000));
 }
