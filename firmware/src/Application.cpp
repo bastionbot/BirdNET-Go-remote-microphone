af5bc0cb898e7aad426e8a3d64f43f9d7b32d81d
bastionbot <droneship16@gmail.com>
2025-08-06 22:13:50 -0400

    Update Application.cpp
    commented out websocket transport

--- firmware/src/Application.cpp
+++ firmware/src/Application.cpp
@@ -1,9 +1,10 @@
+#include <Arduino.h>
 #include <freertos/FreeRTOS.h>
 #include <driver/i2s.h>
 #include "Application.h"
 #include <I2SMEMSSampler.h>
 #include <ADCSampler.h>
-#include "transports/WebSocketTransport.h"
+//#include "transports/WebSocketTransport.h"
 #include "transports/TCPSocketTransport.h"
 #include "config.h"
 
@@ -14,12 +15,12 @@ void Application::begin()
 #else
     this->input = new ADCSampler(ADC_UNIT_1, ADC1_CHANNEL_7, i2s_adc_config);
 #endif
-    this->transport1 = new WebSocketTransport();
+//    this->transport1 = new WebSocketTransport();
     this->transport2 = new TCPSocketTransport();
     this->input->start();
-    this->transport1->begin();
+//    this->transport1->begin();
     this->transport2->begin();
-    
+
     this->taskRunning = true;
     xTaskCreate(Application::streamer_task, "streamer_task", 8192, this, 1, &this->taskHandle);
 }
@@ -27,17 +28,17 @@ void Application::begin()
 Application::~Application()
 {
     Serial.println("[Application] Destructor called - cleaning up resources");
-    
+
     // Signal the task to stop
     this->taskRunning = false;
-    
+
     // Wait for the task to finish (with timeout)
     if (this->taskHandle != nullptr) {
         // Give the task time to exit gracefully
         for (int i = 0; i < 100 && eTaskGetState(this->taskHandle) != eDeleted; i++) {
             vTaskDelay(pdMS_TO_TICKS(10));
         }
-        
+
         // Force delete if still running
         if (eTaskGetState(this->taskHandle) != eDeleted) {
             Serial.println("[Application] Force deleting streaming task");
@@ -45,25 +46,25 @@ Application::~Application()
         }
         this->taskHandle = nullptr;
     }
-    
+
     // Stop and cleanup input
     if (this->input) {
         this->input->stop();
         delete this->input;
         this->input = nullptr;
     }
-    
+
     // Cleanup transports
-    if (this->transport1) {
-        delete this->transport1;
-        this->transport1 = nullptr;
-    }
-    
+//    if (this->transport1) {
+//        delete this->transport1;
+//        this->transport1 = nullptr;
+//    }
+
     if (this->transport2) {
         delete this->transport2;
         this->transport2 = nullptr;
     }
-    
+
     Serial.println("[Application] Cleanup complete");
 }
 
@@ -71,7 +72,7 @@ void Application::streamer_task(void *param)
 {
     Application *app = (Application *)param;
     Serial.println("[Application] Streaming task started");
-    
+
     // Allocate samples buffer
     int16_t *samples = (int16_t *)malloc(sizeof(int16_t) * 1024);
     if (!samples) {
@@ -79,21 +80,21 @@ void Application::streamer_task(void *param)
         vTaskDelete(nullptr);
         return;
     }
-    
+
     while (app->taskRunning)
     {
         // Read from the microphone
         int samples_read = app->input->read(samples, 1024);
-        
+
         // Send to the two transports
         if (samples_read > 0) {
-            app->transport1->send(samples, samples_read * sizeof(int16_t));
+//            app->transport1->send(samples, samples_read * sizeof(int16_t));
             app->transport2->send(samples, samples_read * sizeof(int16_t));
         }
-        
+
         vTaskDelay(pdMS_TO_TICKS(1));
     }
-    
+
     // Cleanup and exit
     free(samples);
     Serial.println("[Application] Streaming task exiting");
